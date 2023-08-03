5804ffb8373c8c331a2cde2ed335010f8fa20e82
fuseMuskan <muskan.bhandari@fusemachines.com>
2023-08-03 14:33:50 +0545

    feat: defined two functions using dictionary comprehension

--- comprehension/main.py
+++ comprehension/main.py
@@ -30,3 +30,19 @@ def zero_sum_triplets(list_one: list, list_two: list, list_three: list) -> list:
     result = [(x, y, z) for x, y, z in zip(list_one, list_two, list_three) if x + y + z == 0]
     result = list(set(result))
     return result
+
+
+# Dictionary Comprehension
+def create_dictionary(keys_list: list, values_list: list) -> dict:
+    """This functions takes two different list one containing keys and
+    another values and returns a dictionary using those two lists.
+
+    Args:
+        keys_list (list): A list containing keys
+        values_list (list): A list containing values
+
+    Returns:
+        result_dict (dict): A dictionary made from keys and values list
+    """
+    result_dict = {key: value for key, value in zip(keys_list, values_list)}
+    return result_dict
