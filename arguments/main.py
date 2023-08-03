1902dbcab9c323d3749644971603ee79da580c9d
fuseMuskan <muskan.bhandari@fusemachines.com>
2023-08-03 10:21:28 +0545

    feat: defined function to create student report

--- arguments/main.py
+++ arguments/main.py
@@ -8,3 +8,23 @@ def sum(*args: int or float) -> int or float:
     for arg in args:
         sum += arg
     return sum
+
+
+def create_student_report(name: str, age: int, **kwargs) -> dict:
+    """_summary_
+
+    Args:
+        name (str): name of the student
+        age (int): age of the student
+
+    Returns:
+        student_report (dict): returns a dictionary with the student's
+        information and a list of subjects along with their scores
+    """
+    student_report = dict()
+    student_report["name"] = name
+    student_report["age"] = age
+    student_report["marks"] = list()
+    for key, value in kwargs.items():
+        student_report["marks"].append({key: value})
+    return student_report
