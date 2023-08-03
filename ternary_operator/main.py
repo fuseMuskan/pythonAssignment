6e42d8cdb1f3e19de1c976e7b129bd3f9de9bd15
fuseMuskan <muskan.bhandari@fusemachines.com>
2023-08-03 12:48:06 +0545

    feat: defined a function to check prime number

--- ternary_operator/main.py
+++ ternary_operator/main.py
@@ -25,8 +25,26 @@ def find_bigger_number(num1: int, num2: int, num3: int) -> int or str:
         num3 (int): any integer
 
     Returns:
-        result (int or str): larger number among num1, num2, and num3 or "Equal"
+        result (int or str): larger number among inputs or "Equal"
     """
 
-    result =  "Equal" if (num1 == num2 & num1 == num3) else num1 if (num1 > num2 and num1 > num3) else num2 if (num2 > num1 and num2 > num3) else num3
+    result = "Equal" if (num1 == num2 and num1 == num3) else num1 if (num1 > num2 and num1 > num3) else num2 if (num2 > num1 and num2 > num3) else num3
     return result
+
+
+def check_prime(num: int) -> str:
+    """This function takes a positive integer as input and
+    uses a ternary operator to determine if it's a prime
+    number.
+
+    Args:
+        num (int): positive integer
+
+    Returns:
+        str: "Prime" or "Not Prime"
+    """
+    flag = 1
+    for i in range(2, num):
+        if num % i == 0:
+            flag = 0
+    return "Prime" if flag else "Not Prime"
