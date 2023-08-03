073dec47dc06dae2d6bf405f53ad40c61a09ffea
fuseMuskan <muskan.bhandari@fusemachines.com>
2023-08-03 19:11:38 +0545

    feat: implemented BankAc and Customer classes

--- oop/main.py
+++ oop/main.py
@@ -1,3 +1,5 @@
+from exception_handling.main import user_age
+
 class University(object):
     def __init__(self, name: str, location: str, departments: list) -> None:
         self._name = name
@@ -17,10 +19,35 @@ class Department(University):
     def display_details(self):
         return f"Department Name: {self.name}, HOD: {self.hod}, Courses: {self.courses}"
 
-university = University("TU", "Kiritipur", ["IOST", "IOE", "IOM"])
-department = Department("IOST", "Muskan Bhandari", ["CSIT", "BCA", "BIT"])
-print(university.display_details())
-print(department.display_details())
+
+class BankAccount(object):
+    def __init__(self, account_number: str, balance: float, account_type: str) -> None:
+        self.account_number = account_number
+        self.balance = balance
+        self.account_type = account_type
+    
+    def __del__(self):
+        print(f"{self.account_number} deleted.")
+
+class Customer(object):
+    def __init__(self, name: str, age: int, address: str, bank_details: BankAccount) -> None:
+        if (user_age(age)):
+            self.name = name
+            self.age = age
+            self.address = address
+            self.bank_details = bank_details
+
+    def __del__(self):
+        print(f"{self.name} account's deleted")
+        super().__del__()
+
+
+        
+
+            
+        
+
+
 
 
         
