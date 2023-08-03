f3732fcb97c37ab2de72ee810d6f22191c482d6e
fuseMuskan <muskan.bhandari@fusemachines.com>
2023-08-03 11:31:06 +0545

    feat: defined a function that returns list of prime numbers

--- functional_programming/main.py
+++ functional_programming/main.py
@@ -1,6 +1,8 @@
 
 def square_numbers(numbers: list) -> list:
-    """_summary_
+    """This function takes a list of integers as input and uses the
+    map function to return a new list containing the square of each
+    element.
 
     Args:
         numbers (list): a list of integers
@@ -10,3 +12,38 @@ def square_numbers(numbers: list) -> list:
     """
     squares = list(map(lambda x: x**2, numbers))
     return squares
+
+
+def prime_number(num: int) -> bool:
+    """This function takes a number a return whether the number
+    is prime number or not
+
+    Args:
+        num (int): any integer
+
+    Returns:
+        result (bool): True (if prime) else False
+    """
+    flag = 1
+    for i in range(2, num):
+        if num % i == 0:
+            flag = 0
+    if flag == 1:
+        return True
+    else:
+        return False
+
+
+def filter_prime_numbers(numbers: list) -> list:
+    """ This function takes a list of integers as input and uses the
+    filter function to return a new list containing only the prime
+    numbers.
+
+    Args:
+        numbers (list): a list of integers
+
+    Returns:
+        primes (list): prime numbers
+    """
+    primes = list(filter(prime_number, numbers))
+    return primes
