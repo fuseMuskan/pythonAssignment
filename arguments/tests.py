afefa9e6fd3abb35d1915fae83ba9b2d537e9833
fuseMuskan <muskan.bhandari@fusemachines.com>
2023-08-03 10:05:04 +0545

    docs: added docstring for unit case

--- arguments/tests.py
+++ arguments/tests.py
@@ -5,6 +5,8 @@ from main import sum
 class TestArgumentFunctions(unittest.TestCase):
 
     def test_sum(self):
+        """Test Cases for sum function
+        """
         self.assertEqual(sum(1), 1)
         self.assertEqual(sum(1, 2, 3, 4, 5), 15)
         self.assertEqual(sum(-1, 5), 4)
